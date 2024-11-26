878d88720756179aa44947b9e3abb789063c8f94
Jens Braun <jens.braun@student.kit.edu>
2024-11-26 20:33:12 +0100

    Finished UFO Parser

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
+use std::path::PathBuf;
 use itertools::Itertools;
+use pyo3::exceptions::{PyIOError, PySyntaxError};
 use thiserror::Error;
-use crate::model::ufoparser::Rule;
+use crate::model::ufoparser::{Rule, UFOParser};
 
 #[cfg(feature = "python-bindings")]
 use pyo3::prelude::*;
@@ -16,6 +18,17 @@ pub enum ModelError {
     IOError(#[from] std::io::Error),
     #[error("Error while parsing UFO model")]
     ParseError(#[from] pest::error::Error<Rule>),
+}
+
+#[cfg(feature = "python-bindings")]
+impl std::convert::From<ModelError> for PyErr {
+    fn from(err: ModelError) -> PyErr {
+        match err {
+            ModelError::IOError(_) => PyIOError::new_err(err.to_string()),
+            ModelError::ParseError(_) => PySyntaxError::new_err(err.to_string()),
+            ModelError::ContentError(_) => PySyntaxError::new_err(err.to_string()),
+        }
+    }
 }
 
 #[derive(PartialEq, Debug, Hash)]
@@ -57,31 +70,29 @@ impl Particle {
 }
 
 #[derive(Debug, PartialEq)]
-pub struct Coupling {
-    name: String,
-    coupling_orders: HashMap<String, isize>
-}
-
-impl Coupling {
-    pub fn new(name: impl Into<String>, coupling_orders: HashMap<String, isize>) -> Self {
-        return Self {
-            name: name.into(),
-            coupling_orders,
-        }
-    }
-}
-
-#[derive(Debug, PartialEq)]
 pub struct Vertex {
     name: String,
-    particles: Vec<Particle>,
-    couplings: Vec<Coupling>,
+    particles: Vec<String>,
+    couplings_orders: HashMap<String, usize>,
 }
 
+#[cfg_attr(feature = "python-bindings", pyclass)]
 #[derive(Debug, PartialEq)]
 pub struct Model {
     particles: HashMap<String, Particle>,
-    vertices: Vec<Vertex>,
+    vertices: HashMap<String, Vertex>,
+    coupling_orders: Vec<String>
+}
+
+#[cfg_attr(feature = "python-bindings", pymethods)]
+impl Model {
+    
+    #[cfg(feature = "python-bindings")]
+    #[pyo3(name = "from_ufo")]
+    #[staticmethod]
+    fn from_ufo_py(path: PathBuf) -> PyResult<Self> {
+        return Ok(UFOParser::parse_ufo_model(&path)?);
+    }
 }
 
 #[cfg_attr(feature = "python-bindings", pyclass)]
@@ -93,7 +104,7 @@ pub struct TopologyModel {
 impl From<&Model> for TopologyModel {
     fn from(model: &Model) -> Self {
         let mut vertex_degrees = Vec::new();
-        for vertex in model.vertices.iter() {
+        for (_, vertex) in model.vertices.iter() {
             vertex_degrees.push(vertex.particles.len());
         }
         return Self {
--- src/model/ufoparser.rs
+++ src/model/ufoparser.rs
@@ -1,15 +1,14 @@
 use std::collections::HashMap;
-use std::error::Error;
 use std::path::PathBuf;
 use pest::Parser;
 use pest_derive::Parser;
-use crate::model::{Coupling, LineStyle, Model, ModelError, Particle, Vertex};
+use crate::model::{LineStyle, Model, ModelError, Particle, Vertex};
 use log::warn;
 use crate::model::ModelError::{ContentError};
 
 #[derive(Parser)]
 #[grammar = "model/ufo_model.pest"]
-struct UFOParser;
+pub(crate) struct UFOParser;
 
 impl UFOParser {
     fn parse_particles(path: &PathBuf) -> Result<HashMap<String, Particle>, ModelError> {
@@ -57,7 +56,6 @@ impl UFOParser {
                             _ => ()
                         }
                     }
-                    println!("{:?}", name);
                     if linestyle == None {
                         if spin == None || color == None {
                             return Err(ContentError(format!("Illegal particle definition in model {:#?} at position {:?}: \
@@ -106,20 +104,162 @@ impl UFOParser {
         return Ok(particles);
     }
 
-    fn parse_coupling_orders(path: &PathBuf) -> Result<Vec<String>, Box<dyn Error>> {
-        todo!();
+    fn parse_coupling_orders(path: &PathBuf) -> Result<Vec<String>, ModelError> {
+        let mut coupling_orders: Vec<String> = Vec::new();
+        let coupling_orders_py_content = std::fs::read_to_string(path.join("coupling_orders.py"))?;
+        let parsed_content = UFOParser::parse(Rule::coupling_orders_py, &coupling_orders_py_content)?.next().unwrap();
+        for coupling_order_rule in parsed_content.into_inner() {
+            match coupling_order_rule.as_rule() {
+                Rule::coupling_order => {
+                    let mut name = String::new();
+                    for property in coupling_order_rule.into_inner() {
+                        match property.as_rule() {
+                            Rule::name => (),
+                            Rule::property_name => {
+                                name = property.into_inner().next().unwrap().as_str()
+                                        .trim_matches(['\"', '\'']).to_string();
+                            },
+                            Rule::property_coupling_order => (),
+                            Rule::property_hierarchy => (),
+                            _ => unreachable!()
+                        }
+                    }
+                    coupling_orders.push(name);
+                },
+                Rule::EOI => (),
+                _ => unreachable!()
+            }
+        }
+        return Ok(coupling_orders);
     }
 
-    fn parse_couplings(path: &PathBuf) -> Result<Vec<Coupling>, Box<dyn Error>> {
-        todo!();
+    fn parse_couplings(path: &PathBuf) -> Result<HashMap<String, HashMap<String, usize>>, ModelError> {
+        let mut couplings: HashMap<String, HashMap<String, usize>> = HashMap::new();
+        let couplings_py_content = std::fs::read_to_string(path.join("couplings.py"))?;
+        let parsed_content = UFOParser::parse(Rule::couplings_py, &couplings_py_content)?.next().unwrap();
+        for couping_rule in parsed_content.into_inner() {
+            match couping_rule.as_rule() {
+                Rule::coupling => {
+                    let mut py_name = String::new();
+                    let mut order: HashMap<String, usize> = HashMap::new();
+                    for property in couping_rule.into_inner() {
+                        match property.as_rule() {
+                            Rule::name => {
+                                py_name = property.as_str().to_string();
+                            }
+                            Rule::property_name => (),
+                            Rule::property_order => {
+                                for entry in property.into_inner() {
+                                    let mut inner_rules = entry.into_inner();
+                                    order.insert(
+                                        inner_rules.next().unwrap().as_str().trim_matches(['\"', '\'']).to_string().to_string(),
+                                        inner_rules.next().unwrap().as_str().parse::<usize>().unwrap()
+                                    );
+                                }
+                            },
+                            Rule::property_value => (),
+                            _ => unreachable!()
+                        }
+                    }
+                    if order.len() != 0 {
+                        couplings.insert(py_name, order);
+                    } else { 
+                        return Err(ContentError(format!("Property 'order' is required for Coupling '{}'", py_name)));
+                    }
+            }
+                Rule::EOI => (),
+                _ => unreachable!()
+            }
+        }
+        return Ok(couplings);
     }
 
-    fn parse_vertices(path: &PathBuf) -> Result<Vec<Vertex>, Box<dyn Error>> {
-        todo!();
+    fn parse_vertices(path: &PathBuf) -> Result<HashMap<String, Vertex>, ModelError> {
+        let mut vertices: HashMap<String, Vertex> = HashMap::new();
+        let coupling_map = Self::parse_couplings(path)?;
+        let vertices_py_content = std::fs::read_to_string(path.join("vertices.py"))?;
+        let parsed_content = UFOParser::parse(Rule::vertices_py, &vertices_py_content)?.next().unwrap();
+        for vertex_rule in parsed_content.into_inner() {
+            match vertex_rule.as_rule() {
+                Rule::vertex => {
+                    let mut name = String::new();
+                    let mut particles: Vec<String> = Vec::new();
+                    let mut coupling_orders_vec = Vec::new();
+                    for property in vertex_rule.into_inner() {
+                        match property.as_rule() {
+                            Rule::name => (),
+                            Rule::property_name => {
+                                name = property.into_inner().next().unwrap().as_str()
+                                        .trim_matches(['\"', '\'']).to_string();
+                            },
+                            Rule::property_particles => {
+                                for particle in property.into_inner() {
+                                    particles.push(particle.as_str().to_string());
+                                }
+                            },
+                            Rule::property_couplings => {
+                                for coupling in property.into_inner() {
+                                    let current_orders = coupling_map.get(&coupling.as_str().to_string())
+                                        .unwrap().clone();
+                                    if !coupling_orders_vec.contains(&current_orders) {
+                                        coupling_orders_vec.push(current_orders);
+                                    }
+                                }
+                                
+                            },
+                            Rule::property_lorentz => (),
+                            Rule::property_vertex_color => (),
+                            _ => unreachable!()
+                        }
+                    }
+                    if particles.len() != 0 {
+                        let n_distinct_orders = coupling_orders_vec.len();
+                        match n_distinct_orders {
+                            0 => return Err(ContentError(format!("Property 'order' required for vertex {}", &name))),
+                            1 => {
+                                vertices.insert(name.clone(),
+                                    Vertex {
+                                        name,
+                                        particles,
+                                        couplings_orders: coupling_orders_vec.pop().unwrap()
+                                    }
+                                );
+                            }
+                            n => {
+                                warn!("Found ambiguous coupling order structure for vertex {}, \
+                                splitting into {} vertices.", &name, n);
+                                for i in 0..n {
+                                    vertices.insert(format!("{}_{}", &name, i),
+                                        Vertex {
+                                            name: format!("{}_{}", &name, i),
+                                            particles: particles.clone(),
+                                            couplings_orders: coupling_orders_vec.pop().unwrap()
+                                        }
+                                    );
+                                }
+                                
+                            }
+                        }
+                    } else {
+                        return Err(ContentError(format!("Property 'particles' required for vertex {}", name)));
+                    }
+                }
+                Rule::EOI => (),
+                _ => unreachable!()
+            }
+        }
+        return Ok(vertices);
     }
 
-    fn parse_ufo_model(path: &PathBuf) -> Result<Model, Box<dyn Error>> {
-        todo!();
+    pub(crate) fn parse_ufo_model(path: &PathBuf) -> Result<Model, ModelError> {
+        let particles = Self::parse_particles(path)?;
+        let coupling_orders = Self::parse_coupling_orders(path)?;
+        let vertices = Self::parse_vertices(path)?;
+        return Ok(Model {
+            particles,
+            vertices,
+            coupling_orders
+        })
     }
 }
 
@@ -127,12 +267,13 @@ impl UFOParser {
 mod tests {
     use std::collections::HashMap;
     use std::path::PathBuf;
-    use crate::model::{Model, ufoparser::UFOParser, Particle, LineStyle};
+    use crate::model::{Model, ufoparser::UFOParser, Particle, Vertex, LineStyle};
 
     #[test]
     fn ufo_parse_test() {
         let path = PathBuf::from("tests/QCD_UFO");
         let model = UFOParser::parse_ufo_model(&path).unwrap();
+        println!("{:#?}", model);
         let model_ref = Model {
             particles: HashMap::from([
                 (String::from("u"), Particle::new(
@@ -149,8 +290,36 @@ mod tests {
                 ))
 
             ]),
-            vertices: Vec::new()
+            vertices: HashMap::from([
+                ("V_1".to_string(), Vertex {
+                    name: "V_1".to_string(),
+                    particles: vec!["G".to_string(); 3],
+                    couplings_orders: HashMap::from([("QCD".to_string(), 1)])
+                }),
+                ("V_2".to_string(), Vertex {
+                    name: "V_2".to_string(),
+                    particles: vec!["G".to_string(); 4],
+                    couplings_orders: HashMap::from([("QCD".to_string(), 2)])
+                }), 
+                ("V_3".to_string(), Vertex {
+                    name: "V_3".to_string(),
+                    particles: vec!["u__tilde__".to_string(), "u".to_string(), "G".to_string()],
+                    couplings_orders: HashMap::from([("QCD".to_string(), 1)])
+                }),
+                ("V_4".to_string(), Vertex {
+                    name: "V_4".to_string(),
+                    particles: vec!["c__tilde__".to_string(), "c".to_string(), "G".to_string()],
+                    couplings_orders: HashMap::from([("QCD".to_string(), 1)])
+                }),
+                ("V_5".to_string(), Vertex {
+                    name: "V_5".to_string(),
+                    particles: vec!["t__tilde__".to_string(), "t".to_string(), "G".to_string()],
+                    couplings_orders: HashMap::from([("QCD".to_string(), 1)])
+                })
+            ]),
+            coupling_orders: vec!["QCD".to_string()],
         };
+        println!("{:#?}", model_ref);
         assert_eq!(model, model_ref);
     }
 }
