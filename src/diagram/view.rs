Jens-Braun/FeynGraph

commit f5f74a7b069967f20662acd52bb98751cb655395

Fixed accidental double propagator inversion in 'ray_index_ordered' and implemented some missing public functions

src/diagram/view.rs
@@ -495,16 +495,15 @@ impl PropagatorView<'_> {
     /// leg of the `index`-th vertex to which the propagator is connected to. The ray index is given with respect to the
     /// propagators ordered as in the interaction vertex.
     pub fn ray_index_ordered(&self, index: usize) -> usize {
-        let i = if self.invert { 1 - index } else { index };
         return self
-            .vertex(i)
+            .vertex(index)
             .propagators_ordered()
             .position(|p| {
                 if let either::Right(p) = p
                     && p.index == self.index
                 {
                     if self.propagator.vertices[0] == self.propagator.vertices[1] {
-                        if i == 1 {
+                        if index == 1 {
                             return p.particle().is_anti();
                         } else {
                             return !p.particle().is_anti();

python/feyngraph/feyngraph.pyi
@@ -115,6 +115,14 @@ class Leg:
         signature identical to `Propagator.ray_index`, but the parameter is always ignored.
         """
 
+    def ray_index_ordered(self, _vertex: int = 0) -> int:
+        """
+        Get the external leg's ray index, i.e. the index of the leg of the vertex to which the external leg is
+        connected to (_from the vertex perspective_). This function accepts an addition `_vertex` parameter to make its
+        signature identical to `Propagator.ray_index`, but the parameter is always ignored. The ray index is given with
+        respect to the propagators ordered as in the interaction vertex.
+        """
+
     def id(self) -> int:
         """Get the leg's internal id"""
 
@@ -157,6 +165,13 @@ class Propagator:
         """
         Get the propagators ray index with respect to the `index`-th vertex it is connected to, i.e. the index of the
         leg of the `index`-th vertex to which the propagator is connected to.
+        """
+
+    def ray_index_ordered(self, index: int) -> int:
+        """
+        Get the propagators ray index with respect to the `index`-th vertex it is connected to, i.e. the index of the
+        leg of the `index`-th vertex to which the propagator is connected to. The ray index is given with respect to
+        the propagators ordered as in the interaction vertex.
         """
 
     def id(self) -> int:

src/bindings/python/diagrams.rs
@@ -92,6 +92,23 @@ impl PyLeg {
             .unwrap();
     }
 
+    #[pyo3(signature = (_vertex = 0))]
+    pub fn ray_index_ordered(&self, _vertex: usize) -> usize {
+        return self
+            .diagram
+            .vertex(self.leg.vertex)
+            .propagators_ordered()
+            .iter()
+            .position(|p| {
+                if let either::Left(l) = p {
+                    l.id() == self.leg_index
+                } else {
+                    false
+                }
+            })
+            .unwrap();
+    }
+
     pub fn id(&self) -> usize {
         return self.leg_index;
     }
@@ -269,16 +286,15 @@ impl PyPropagator {
     }
 
     pub fn ray_index_ordered(&self, index: usize) -> usize {
-        let i = if self.invert { 1 - index } else { index };
         return self
-            .vertex(i)
+            .vertex(index)
             .propagators_ordered()
             .iter()
             .position(|p| {
                 if let either::Right(p) = p {
                     if p.index == self.index {
                         if self.propagator.vertices[0] == self.propagator.vertices[1] {
-                            if i == 1 {
+                            if index == 1 {
                                 return p.particle().is_anti();
                             } else {
                                 return !p.particle().is_anti();

src/bindings/python/mod.rs
@@ -173,6 +173,10 @@ impl PyParticle {
         return self.0.is_fermi();
     }
 
+    pub(crate) fn self_anti(&self) -> bool {
+        return self.0.self_anti();
+    }
+
     pub(crate) fn pdg(&self) -> isize {
         return self.0.pdg();
     }

commit 0a9c9f1d49f395835151363a379fe83e2ed39266

Fixed a bug in the diagram sign calculation introduced in f5f74a7b069967f20662acd52bb98751cb655395

src/diagram/view.rs
@@ -181,7 +181,9 @@ impl<'a> DiagramView<'a> {
                         continue;
                     }
                     visited_props[prop.index] = true;
-                    if prop.propagator.vertices[0] == current.index {
+                    if (prop.propagator.vertices[0] == current.index && !prop.invert)
+                        || (prop.propagator.vertices[1] == current.index && prop.invert)
+                    {
                         in_ray = prop.ray_index_ordered(1);
                         to_visit.push(self.vertex(prop.propagator.vertices[1]));
                     } else {